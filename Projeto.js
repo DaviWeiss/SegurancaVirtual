40d3f670be6ea03f0f8a7ffec74bc4828306da10
Davi de Baptista Weiss <69022428+DaviWeiss@users.noreply.github.com>
2021-05-20 21:00:46 -0300

    Update Projeto.js

--- Projeto.js
+++ Projeto.js
@@ -1,1 +1,10 @@
-
+function VerificarEntrada(){
+    NomeConvidado = document.getElementById('nome').value;
+    ConvidadosDavi = ['Rafela', 'Davi', 'Rafael', 'Renata', 'Alexandre', 'Gabriela', 'Isabela', 'Marcela', 'Sophia', 'Giovanna'];
+    if(ConvidadosDavi.includes(NomeConvidado)){
+        document.getElementById('permissaoEntrada').innerText = 'Você pode entrar!'
+    }
+    else {
+        document.getElementById('permissaoEntrada').innerText = 'Você não pode entrar!'
+    }
+}
